e6acf1d898c7838aaf0483acd995f28996ae1396
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2023-12-11 23:02:26 +0300

    Compelte home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Job\Job;
 class HomeController extends Controller
 {
     /**
@@ -23,6 +23,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $jobs = Job::select()->take(5)->orderby('id','desc')->get();
+        $totalJobs = Job::all()->count();
+
+        return view('home',compact('jobs','totalJobs'));
     }
 }
